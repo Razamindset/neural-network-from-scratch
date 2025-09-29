f610335844ec9361fc9ff4d7f8022bf0c58b2ad0
Ali Raza Khalid <razamindset.official@gmail.com>
2025-09-29 22:04:35 +0500

    Init: CNN

---
+++ src/cnn/CNN.py
@@ -0,0 +1,15 @@
+import numpy as np
+from scipy import signal
+
+class Convolution:
+    def __init__(self):
+        pass
+
+
+class MaxPolling:
+    def __init__(self):
+        pass
+
+class Dense:
+    def __init__(self):
+        pass
